9c25ca2d5bcb560baec7121e7f8ec4a6dab62f0a
SiCheng Meng <km883@outlook.com>
2022-04-01 03:10:41 -0400

    emptySpaceExists() done.

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author SiCheng Meng
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 1; i < b.size(); i++) {
+            for (int j = 1; j < b.size(); j++) {
+                if (b.tile(i, j) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 

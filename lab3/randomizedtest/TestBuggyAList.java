7d0308174de352e9dbf67dab767feea1c4cabf10
SiCheng Meng <km883@outlook.com>
2022-04-18 02:42:56 -0400

    completed randomizedTest v1.

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -28,4 +28,31 @@ public class TestBuggyAList {
         assertEquals(good.removeLast(), buggy.removeLast());
 
     }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> good = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                good.addLast(randVal);
+                buggy.addLast(randVal);
+                // System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                assertEquals(good.size(), buggy.size());
+            } else if (operationNumber == 2 && good.size() > 0) {
+                // getLast
+                assertEquals(good.getLast(), buggy.getLast());
+            } else if (operationNumber == 3 && good.size() > 0) {
+                //removeLast
+                assertEquals(good.removeLast(), buggy.removeLast());
+            }
+        }
+    }
 }

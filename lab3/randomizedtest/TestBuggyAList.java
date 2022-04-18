de43557579924441b13eef251b2ef76f86f5770d
SiCheng Meng <km883@outlook.com>
2022-04-18 00:03:11 -0400

    Simple Comparison Test done.

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,5 +1,6 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,4 +10,22 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> good = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+
+        good.addLast(4);
+        good.addLast(5);
+        good.addLast(6);
+        buggy.addLast(4);
+        buggy.addLast(5);
+        buggy.addLast(6);
+
+        assertEquals(good.size(), buggy.size());
+        assertEquals(good.removeLast(), buggy.removeLast());
+        assertEquals(good.removeLast(), buggy.removeLast());
+        assertEquals(good.removeLast(), buggy.removeLast());
+
+    }
 }
